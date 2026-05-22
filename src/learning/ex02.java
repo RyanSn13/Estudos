9e90275def65ae3e9c1ab94e9ca18f2656dedac6
RyanSn13 <ryansenasamuel@gmail.com>
2026-05-22 11:24:50 -0300

    Upsdate

--- src/learning/ex02.java
+++ src/learning/ex02.java
@@ -1,4 +1,22 @@
 package learning;
 
+import java.util.Scanner;
+
 public class ex02 {
+    static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        ex02_1 product = new ex02_1();
+        System.out.println("Enter product data -  ");
+        System.out.print("Name: ");
+
+        product.name = leitor.nextLine();
+        System.out.print("Price: ");
+        product.price = leitor.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        product.quantity = leitor.nextInt();
+
+    }
+
 }
--- src/learning/ex02_1.java
+++ src/learning/ex02_1.java
@@ -1,4 +1,17 @@
 package learning;
 
 public class ex02_1 {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueinStock(){
+        return price * quantity;
+    }
+     public void addProducts(int quantity){
+        this.quantity += quantity;
+     }
+     public void removeProducts(int quantity){
+        this.quantity -= quantity;
+     }
 }
